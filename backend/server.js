cc16f2e93229d5f7b6cdf904c2743ebecfc7a385
sa-gar <67649354+sa-gar@users.noreply.github.com>
2026-03-21 13:35:05 +0530

    add admin auth route

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use("/api/auth", authRoutes);
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -24,6 +23,7 @@ app.get("/", (req, res) => {
 app.use("/api/tutors", tutorRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/enquiries", enquiryRoutes);
+app.use("/api/auth", authRoutes);
 
 if (process.env.MONGO_URI && process.env.MONGO_URI !== "your_mongodb_connection_string") {
   mongoose
