6a29948270ec3ba212418fd322146aaf0135fd83
sa-gar <67649354+sa-gar@users.noreply.github.com>
2026-03-31 00:40:02 +0530

    Update Blog.js

--- backend/models/Blog.js
+++ backend/models/Blog.js
@@ -4,7 +4,7 @@ const blogSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     content: { type: String, required: true },
-    image: { type: String },
+    image: { type: String, default: "" },
     author: { type: String, default: "Admin" },
   },
   { timestamps: true }
