922d99bca357a34566a8d22d6f90f47934a5385d
sa-gar <67649354+sa-gar@users.noreply.github.com>
2026-05-03 16:09:17 +0530

    Update ParentEnquiry.js

--- backend/models/ParentEnquiry.js
+++ backend/models/ParentEnquiry.js
@@ -5,6 +5,7 @@ const wardSchema = new mongoose.Schema(
     wardName: { type: String, required: true, trim: true },
     schoolName: { type: String, default: "", trim: true },
     classGrade: { type: String, required: true, trim: true },
+curriculum: { type: String, default: "", trim: true },
    subjectsNeeded: {
   type: [String],   // ✅ array of strings
   required: true,
